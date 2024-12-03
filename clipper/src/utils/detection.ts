9443ceee49426d972f9312fdee62ada767ad465f
nathan <nathan@appflowy.io>
2024-12-03 19:30:32 +0800

    chore: fix wanrings

--- clipper/src/utils/detection.ts
+++ clipper/src/utils/detection.ts
@@ -4,13 +4,6 @@ interface NavigatorExtended extends Navigator {
   };
 }
 
-function isIPad(): boolean {
-  return (
-    navigator.userAgent.includes('iPad') ||
-    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)
-  );
-}
-
 export async function detectBrowser(): Promise<
   | 'chrome'
   | 'firefox'
@@ -19,7 +12,6 @@ export async function detectBrowser(): Promise<
   | 'edge'
   | 'safari'
   | 'mobile-safari'
-  | 'ipad-os'
   | 'other'
 > {
   const userAgent = navigator.userAgent.toLowerCase();
@@ -36,9 +28,7 @@ export async function detectBrowser(): Promise<
     }
     return 'chrome';
   } else if (userAgent.includes('safari')) {
-    if (isIPad()) {
-      return 'ipad-os';
-    } else if (userAgent.includes('mobile') || userAgent.includes('iphone')) {
+    if (userAgent.includes('mobile') || userAgent.includes('iphone')) {
       return 'mobile-safari';
     }
     return 'safari';
